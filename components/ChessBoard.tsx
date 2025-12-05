94cce2113593a49a922ad2f8f27cf5576671f211
Prathmesh Ravindra Salunkhe <salunkheprathmesh0@gmail.com>
2025-12-05 15:17:13 +0530

    pakage updates

--- components/ChessBoard.tsx
+++ components/ChessBoard.tsx
@@ -1,11 +1,12 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Chessboard } from "react-chessboard";
 import { Chess } from "chess.js";
 
 const ChessBoard = ({ pgn }: { pgn?: string }) => {
   const [game, setGame] = useState(new Chess());
+  const [moves, setMoves] = useState<string[][]>([]);
 
   // Load PGN if provided
   if (pgn) {
@@ -26,6 +27,18 @@ const ChessBoard = ({ pgn }: { pgn?: string }) => {
     return true;
   };
 
+  useEffect(() => {
+    const history = game.history();
+    const moves: string[][] = [];
+
+    for(let i = 1; i < history.length; i+=2) {
+      const m = [history[i-1], history[i]];
+      moves.push(m);
+    }
+    setMoves(moves);
+    //console.log("Moves history:", moves);
+  }, [game]);
+
   const chessboardOptions = {
     // your config options here
     position: game.fen(),
@@ -33,8 +46,8 @@ const ChessBoard = ({ pgn }: { pgn?: string }) => {
       display: "grid",
       gridTemplateColumns: `repeat(${8}, 1fr)`,
       overflow: "hidden",
-      width: '100%',
-      height: '100%',
+      width: '400px',
+      height: '400px',
       position: 'relative',
     },
     pieceStyle: {
@@ -45,10 +58,24 @@ const ChessBoard = ({ pgn }: { pgn?: string }) => {
   };
 
 return (
-  <div className="flex flex-col items-center">
-    <Chessboard
+  <div className="flex items-center">
+    <section className="flex flex-col items-center m-2">
+      <Chessboard
       options={chessboardOptions}
     />
+    </section>
+    
+  
+    <section className="bg-slate-900 text-white h-max p-4">
+      <h2 className="text-center font-bold mb-2">Moves History</h2>
+      <ol className="list-decimal list-inside scroll-m-0 h-96 overflow-y-auto">
+        {moves.map((movePair, index) => (
+          <li key={index} className="w-80">
+            {movePair[0]    } {movePair[1] ? movePair[1] : ""}
+          </li>
+        ))}
+      </ol>
+    </section>
   </div>
 );
 };
